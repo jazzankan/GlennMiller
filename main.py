a5ceff9f06fe963999f102e7786c34fb52b638b2
anders <anders@webbsallad.se>
2024-09-20 12:52:02 +0200

    Första commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://www.glennmillercafe.se/konserter")
+glenn_page = response.text
+soup = BeautifulSoup(glenn_page, "html.parser")
+
+
